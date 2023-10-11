0864057f08223bb56452bd87273c60aba04c17af
sadmann7 <sadmann898@gmail.com>
2023-10-12 03:02:30 +0600

    add data-table-floating-bar

--- src/app/_actions/task.ts
+++ src/app/_actions/task.ts
@@ -7,7 +7,11 @@ import { faker } from "@faker-js/faker"
 import { eq } from "drizzle-orm"
 import type { z } from "zod"
 
-import type { updateTaskLabelSchema } from "@/lib/validations/task"
+import type {
+  updateTaskLabelSchema,
+  updateTaskPrioritySchema,
+  updateTaskStatusSchema,
+} from "@/lib/validations/task"
 
 export async function seedTasksAction() {
   const allTasks: Task[] = []
@@ -39,6 +43,28 @@ export async function updateTaskLabelAction({
   label,
 }: z.infer<typeof updateTaskLabelSchema>) {
   await db.update(tasks).set({ label }).where(eq(tasks.id, id))
+
+  revalidatePath("/")
+}
+
+export async function updateTaskStatusAction({
+  id,
+  status,
+}: z.infer<typeof updateTaskStatusSchema>) {
+  console.log("updateTaskStatusAction", id, status)
+
+  await db.update(tasks).set({ status }).where(eq(tasks.id, id))
+
+  revalidatePath("/")
+}
+
+export async function updateTaskPriorityAction({
+  id,
+  priority,
+}: z.infer<typeof updateTaskPrioritySchema>) {
+  console.log("updatePriorityAction", id, priority)
+
+  await db.update(tasks).set({ priority }).where(eq(tasks.id, id))
 
   revalidatePath("/")
 }
