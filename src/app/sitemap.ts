16ceb3b86e80a2719527a6b8cd954c08014921c4
sadmann7 <sadmann898@gmail.com>
2024-01-02 00:08:14 +0600

    deubub build error

---
+++ src/app/sitemap.ts
@@ -0,0 +1,12 @@
+import { type MetadataRoute } from "next"
+
+import { siteConfig } from "@/config/site"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const routes = [""].map((route) => ({
+    url: `${siteConfig.url}${route}`,
+    lastModified: new Date().toISOString(),
+  }))
+
+  return [...routes]
+}
