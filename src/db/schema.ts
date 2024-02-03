fa5c212ada63cf184442e3121f47f80f9dea6713
sadmann7 <sadmann898@gmail.com>
2024-02-03 21:39:56 +0600

    Add multi-project schema example to schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,6 +2,13 @@ import { mysqlEnum, mysqlTableCreator, varchar } from "drizzle-orm/mysql-core"
 
 import { createId } from "@/lib/utils"
 
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+
 export const mysqlTable = mysqlTableCreator((name) => `shadcn-table_${name}`)
 
 export const tasks = mysqlTable("tasks", {
