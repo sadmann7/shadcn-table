0d5e36da4c27f1857934f618f0ccb1e9009b6fed
sadmann7 <sadmann898@gmail.com>
2024-03-15 04:29:07 +0600

    chore: sync

--- src/components/data-table/advanced/data-table-advanced-filter-item.tsx
+++ src/components/data-table/advanced/data-table-advanced-filter-item.tsx
@@ -138,8 +138,8 @@ export function DataTableAdvancedFilterItem<TData>({
           )}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-60 space-y-1 p-1.5 text-xs" align="start">
-        <div className="flex items-center space-x-1 px-1 pt-0.5">
+      <PopoverContent className="w-60 space-y-1.5 p-2 text-xs" align="start">
+        <div className="flex items-center space-x-1 pl-1 pr-0.5">
           <div className="flex flex-1 items-center space-x-1">
             <div className="capitalize">{selectedOption.label}</div>
             <Select onValueChange={(value) => setFilterVariety(value)}>
