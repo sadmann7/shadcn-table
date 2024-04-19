4f6fc5487ddae4d3891eb2b08ce98689a80c06f4
sadmann7 <sadmann898@gmail.com>
2024-04-19 16:52:10 +0600

    feat: update toggle-button and downgrade pnpm

--- src/components/toggle-button.tsx
+++ src/components/toggle-button.tsx
@@ -1,27 +1,63 @@
 "use client"
 
-import * as React from "react"
 
-import { cn, composeEventHandlers } from "@/lib/utils"
 import { Button, type ButtonProps } from "@/components/ui/button"
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
+import { useControllableState } from "@/hooks/use-controllable-state"
+import { cn, composeEventHandlers } from "@/lib/utils"
 
 interface ToggleButtonProps extends ButtonProps {
+  /**
+   * The controlled state of the toggle.
+   */
+  pressed?: boolean;
+
+  /**
+   * The state of the toggle when initially rendered. Use `defaultPressed`
+   * if you do not need to control the state of the toggle.
+   * @defaultValue false
+   */
+  defaultPressed?: boolean;
+
+  /**
+   * The callback that fires when the state of the toggle changes.
+   */
+  onPressedChange?(pressed: boolean): void;
+
+  /**
+   * The title of the tooltip.
+   * @defaultValue undefined
+   * @example "Enable notifications"
+   */
+
   tooltipTitle?: string
+
+  /**
+   * The description of the tooltip.
+   * @defaultValue undefined
+   * @example "Receive notifications when someone mentions you"
+   */
   tooltipDescription?: string
 }
 
 export function ToggleButton({
+  pressed: pressedProp,
+  defaultPressed = false,
+  onPressedChange,
   tooltipTitle,
   tooltipDescription,
   className,
   ...props
 }: ToggleButtonProps) {
-  const [pressed, setPressed] = React.useState(false)
+  const [pressed, setPressed] = useControllableState({
+    prop: pressedProp,
+    defaultProp: defaultPressed,
+    onChange: onPressedChange,
+  })
 
   return (
     <Tooltip>
