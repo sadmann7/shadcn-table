657a71691c61acc6513e7176cec39392899b0587
sadmann7 <sadmann898@gmail.com>
2024-04-26 08:11:32 +0600

    feat: remove icon.tsx

--- src/components/shell.tsx
+++ src/components/shell.tsx
@@ -8,8 +8,8 @@ const shellVariants = cva("grid items-center gap-8 pb-8 pt-6 md:py-8", {
     variant: {
       default: "container",
       sidebar: "",
-      centered: "mx-auto mb-16 mt-20 max-w-md justify-center",
-      markdown: "container max-w-3xl gap-0 py-8 md:py-10 lg:py-10",
+      centered: "container flex h-dvh max-w-2xl flex-col justify-center py-16",
+      markdown: "container max-w-3xl py-8 md:py-10 lg:py-10",
     },
   },
   defaultVariants: {
--- src/env.js
+++ src/env.js
@@ -11,8 +11,6 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
     DATABASE_URL: z.string().url(),
-    UPSTASH_REDIS_REST_URL: z.string().url(),
-    UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
   },
 
   /**
@@ -31,8 +29,6 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
