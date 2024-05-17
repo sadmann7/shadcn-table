3d7103a2ff43824e348313234e6251acef288fb5
sadmann7 <sadmann898@gmail.com>
2024-05-17 20:56:16 +0600

    feat: update drizzle scripts

--- drizzle/0001_magenta_red_wolf.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "shadcn_tasks" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "shadcn_tasks" ADD COLUMN "updated_at" timestamp DEFAULT current_timestamp;
--- drizzle/0002_fat_strong_guy.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "shadcn_tasks" ALTER COLUMN "id" SET DATA TYPE varchar(30);--> statement-breakpoint
-ALTER TABLE "shadcn_tasks" ALTER COLUMN "code" SET DATA TYPE varchar(256);--> statement-breakpoint
-ALTER TABLE "shadcn_tasks" ALTER COLUMN "title" SET DATA TYPE varchar(256);
