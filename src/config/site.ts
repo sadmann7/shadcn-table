dd349d4a04754cd070ffeb426fe95a7ec64d73c8
sadmann7 <sadmann898@gmail.com>
2024-07-28 06:42:14 +0600

    chore: update config

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,7 +5,7 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "Table",
   description:
-    "Shadcn table component with server side sorting, pagination, and filtering",
+    "Shadcn table with server side sorting, pagination, and filtering",
   url:
     env.NODE_ENV === "development"
       ? "http://localhost:3000"
