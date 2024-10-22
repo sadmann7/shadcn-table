0d702f7b4874b789cd27d32a3a4275c4a8a306e5
sadmann7 <sadmann898@gmail.com>
2024-10-22 08:35:26 +0600

    chore: next 15

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ await import("./src/env.js")
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  experimental: {
+    ppr: true,
+  },
   // Already doing linting and typechecking as separate tasks in CI
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
