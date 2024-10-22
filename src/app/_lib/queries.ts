a959721381eef876240acb76e7a0d243ef5c3ce4
sadmann7 <sadmann898@gmail.com>
2024-10-22 08:13:22 +0600

    feat: cache queries

--- src/app/_lib/queries.ts
+++ src/app/_lib/queries.ts
@@ -1,168 +1,110 @@
 import "server-only"
 
-import {
-  unstable_cache as cache,
-  unstable_noStore as noStore,
-} from "next/cache"
 import { db } from "@/db"
 import { tasks, type Task } from "@/db/schema"
-import {
-  and,
-  asc,
-  count,
-  desc,
-  eq,
-  gt,
-  gte,
-  lte,
-  or,
-  type SQL,
-} from "drizzle-orm"
+import { and, asc, count, desc, gt, gte, lte, or, type SQL } from "drizzle-orm"
 
 import { filterColumn } from "@/lib/filter-column"
+import { unstable_cache } from "@/lib/unstable-cache"
 
 import { type GetTasksSchema } from "./validations"
 
 export async function getTasks(input: GetTasksSchema) {
-  noStore()
+  return await unstable_cache(
+    async () => {
+      try {
+        // Offset to paginate the results
+        const offset = (input.page - 1) * input.per_page
+        // Column and order to sort by
+        // Spliting the sort string by "." to get the column and order
+        // Example: "title.desc" => ["title", "desc"]
+        const [column, order] = (input.sort?.split(".").filter(Boolean) ?? [
+          "createdAt",
+          "desc",
+        ]) as [keyof Task | undefined, "asc" | "desc" | undefined]
 
-  try {
-    // Offset to paginate the results
-    const offset = (input.page - 1) * input.per_page
-    // Column and order to sort by
-    // Spliting the sort string by "." to get the column and order
-    // Example: "title.desc" => ["title", "desc"]
-    const [column, order] = (input.sort?.split(".").filter(Boolean) ?? [
-      "createdAt",
-      "desc",
-    ]) as [keyof Task | undefined, "asc" | "desc" | undefined]
+        // Convert the date strings to date objects
+        const fromDate = input.from ? new Date(input.from) : undefined
+        const toDate = input.to ? new Date(input.to) : undefined
 
-    // Convert the date strings to date objects
-    const fromDate = input.from ? new Date(input.from) : undefined
-    const toDate = input.to ? new Date(input.to) : undefined
+        const expressions: (SQL<unknown> | undefined)[] = [
+          input.title
+            ? filterColumn({
+                column: tasks.title,
+                value: input.title,
+              })
+            : undefined,
+          // Filter tasks by status
+          !!input.status
+            ? filterColumn({
+                column: tasks.status,
+                value: input.status,
+                isSelectable: true,
+              })
+            : undefined,
+          // Filter tasks by priority
+          !!input.priority
+            ? filterColumn({
+                column: tasks.priority,
+                value: input.priority,
+                isSelectable: true,
+              })
+            : undefined,
+          // Filter by createdAt
+          fromDate ? gte(tasks.createdAt, fromDate) : undefined,
+          toDate ? lte(tasks.createdAt, toDate) : undefined,
+        ]
+        const where =
+          !input.operator || input.operator === "and"
+            ? and(...expressions)
+            : or(...expressions)
 
-    const expressions: (SQL<unknown> | undefined)[] = [
-      input.title
-        ? filterColumn({
-            column: tasks.title,
-            value: input.title,
-          })
-        : undefined,
-      // Filter tasks by status
-      !!input.status
-        ? filterColumn({
-            column: tasks.status,
-            value: input.status,
-            isSelectable: true,
-          })
-        : undefined,
-      // Filter tasks by priority
-      !!input.priority
-        ? filterColumn({
-            column: tasks.priority,
-            value: input.priority,
-            isSelectable: true,
-          })
-        : undefined,
-      // Filter by createdAt
-      fromDate ? gte(tasks.createdAt, fromDate) : undefined,
-      toDate ? lte(tasks.createdAt, toDate) : undefined,
-    ]
-    const where =
-      !input.operator || input.operator === "and"
-        ? and(...expressions)
-        : or(...expressions)
+        // Transaction is used to ensure both queries are executed in a single transaction
+        const { data, total } = await db.transaction(async (tx) => {
+          const data = await tx
+            .select()
+            .from(tasks)
+            .limit(input.per_page)
+            .offset(offset)
+            .where(where)
+            .orderBy(
+              column && column in tasks
+                ? order === "asc"
+                  ? asc(tasks[column])
+                  : desc(tasks[column])
+                : desc(tasks.id)
+            )
 
-    // Transaction is used to ensure both queries are executed in a single transaction
-    const { data, total } = await db.transaction(async (tx) => {
-      const data = await tx
-        .select()
-        .from(tasks)
-        .limit(input.per_page)
-        .offset(offset)
-        .where(where)
-        .orderBy(
-          column && column in tasks
-            ? order === "asc"
-              ? asc(tasks[column])
-              : desc(tasks[column])
-            : desc(tasks.id)
-        )
+          const total = await tx
+            .select({
+              count: count(),
+            })
+            .from(tasks)
+            .where(where)
+            .execute()
+            .then((res) => res[0]?.count ?? 0)
 
-      const total = await tx
-        .select({
-          count: count(),
+          return {
+            data,
+            total,
+          }
         })
-        .from(tasks)
-        .where(where)
-        .execute()
-        .then((res) => res[0]?.count ?? 0)
 
-      return {
-        data,
-        total,
-      }
-    })
-
-    const pageCount = Math.ceil(total / input.per_page)
-    return { data, pageCount }
-  } catch (err) {
-    return { data: [], pageCount: 0 }
-  }
-}
-
-export async function getTaskCountByStatus(status: Task["status"]) {
-  return cache(
-    async () => {
-      try {
-        return await db
-          .selectDistinct({
-            count: count(),
-          })
-          .from(tasks)
-          .where(eq(tasks.status, status))
-          .groupBy(tasks.status)
-          .having(gt(count(tasks.status), 0))
-          .then((res) => res[0]?.count ?? 0)
+        const pageCount = Math.ceil(total / input.per_page)
+        return { data, pageCount }
       } catch (err) {
-        return 0
+        return { data: [], pageCount: 0 }
       }
     },
-    [`task-status-${status}`],
+    ["tasks"],
     {
-      revalidate: 900,
-      tags: ["task-count", `task-status-${status}`],
-    }
-  )()
-}
-
-export async function getTaskCountByPriority(priority: Task["priority"]) {
-  return cache(
-    async () => {
-      try {
-        return await db
-          .selectDistinct({
-            count: count(),
-          })
-          .from(tasks)
-          .where(eq(tasks.priority, priority))
-          .groupBy(tasks.priority)
-          .having(gt(count(tasks.priority), 0))
-          .then((res) => res[0]?.count ?? 0)
-      } catch (err) {
-        return 0
-      }
-    },
-    [`task-priority-${priority}`],
-    {
-      revalidate: 900,
-      tags: ["task-count", `task-priority-${priority}`],
+      revalidate: 3600,
     }
   )()
 }
 
 export async function getTaskStatusCounts() {
-  return cache(
+  return unstable_cache(
     async () => {
       try {
         return await db
@@ -188,14 +130,13 @@ export async function getTaskStatusCounts() {
     },
     ["task-status-counts"],
     {
-      revalidate: 900,
-      tags: ["task-count", "task-status-counts"],
+      revalidate: 3600,
     }
   )()
 }
 
 export async function getTaskPriorityCounts() {
-  return cache(
+  return unstable_cache(
     async () => {
       try {
         return await db
@@ -221,8 +162,7 @@ export async function getTaskPriorityCounts() {
     },
     ["task-priority-counts"],
     {
-      revalidate: 900,
-      tags: ["task-count", "task-priority-counts"],
+      revalidate: 3600,
     }
   )()
 }
