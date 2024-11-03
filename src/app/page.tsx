0cbc4401319efbbeef9ef9aa93a4bfc1b4d66db5
sadmann7 <sadmann898@gmail.com>
2024-11-03 15:08:31 +0600

    chore: sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,8 +35,6 @@ export default async function IndexPage(props: IndexPageProps) {
     getTaskPriorityCounts(),
   ])
 
-  console.log({ search })
-
   return (
     <Shell className="gap-2">
       <FeatureFlagsProvider>
