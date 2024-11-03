e043994c14a1abb62f219cb01f366bf848d62600
sadmann7 <sadmann898@gmail.com>
2024-11-04 00:45:03 +0600

    feat: optimize sorting

--- src/components/data-table/data-table-filter-list.tsx
+++ src/components/data-table/data-table-filter-list.tsx
@@ -143,6 +143,16 @@ export function DataTableFilterList<TData>({
     void setFilters(updatedFilters)
   }
 
+  function moveFilter(activeIndex: number, overIndex: number) {
+    void setFilters((prevFilters) => {
+      const newFilters = [...prevFilters]
+      const [removed] = newFilters.splice(activeIndex, 1)
+      if (!removed) return prevFilters
+      newFilters.splice(overIndex, 0, removed)
+      return newFilters
+    })
+  }
+
   function renderFilterInput({
     filter,
     inputId,
@@ -493,12 +503,9 @@ export function DataTableFilterList<TData>({
   return (
     <Sortable
       value={filters.map((item) => ({ id: item.rowId }))}
-      onValueChange={(items) => {
-        const ids = new Set(items.map((item) => item.id))
-        void setFilters((prevFilters) =>
-          prevFilters.filter((filter) => ids.has(filter.rowId))
-        )
-      }}
+      onMove={({ activeIndex, overIndex }) =>
+        moveFilter(activeIndex, overIndex)
+      }
       overlay={
         <div className="flex items-center gap-2">
           <div className="h-8 min-w-[4.5rem] rounded-sm bg-primary/10" />
