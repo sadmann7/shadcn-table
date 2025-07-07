84ad197680003ac7efcce2d3656131b36f7ee19b
sadmann7 <sadmann898@gmail.com>
2025-07-07 09:18:03 +0600

    chore: update css variables

--- src/lib/data-table.ts
+++ src/lib/data-table.ts
@@ -22,16 +22,16 @@ export function getCommonPinningStyles<TData>({
   return {
     boxShadow: withBorder
       ? isLastLeftPinnedColumn
-        ? "-4px 0 4px -4px hsl(var(--border)) inset"
+        ? "-4px 0 4px -4px var(--border) inset"
         : isFirstRightPinnedColumn
-          ? "4px 0 4px -4px hsl(var(--border)) inset"
+          ? "4px 0 4px -4px var(--border) inset"
           : undefined
       : undefined,
     left: isPinned === "left" ? `${column.getStart("left")}px` : undefined,
     right: isPinned === "right" ? `${column.getAfter("right")}px` : undefined,
     opacity: isPinned ? 0.97 : 1,
     position: isPinned ? "sticky" : "relative",
-    background: isPinned ? "hsl(var(--background))" : "hsl(var(--background))",
+    background: isPinned ? "var(--background)" : "var(--background)",
     width: column.getSize(),
     zIndex: isPinned ? 1 : 0,
   };
